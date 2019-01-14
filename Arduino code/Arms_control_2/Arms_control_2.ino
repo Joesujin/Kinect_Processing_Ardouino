fb7599d527aca8b9febb271da6be7b41186918b8
Joesujin <joesujin@gmail.com>
2019-01-14 13:25:37 +0530

    Arduino controllers

---
+++ Arduino code/Arms_control_2/Arms_control_2.ino
@@ -0,0 +1,55 @@
+#include<Servo.h>
+Servo servoX;
+Servo servoY;
+int state;
+int posX = 0;
+int posY = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  servoX.attach(9);
+  servoY.attach(10);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (Serial.available() > 0)
+  {
+    state = Serial.read();
+    if (state == '1')
+    {
+      digitalWrite(13, HIGH);
+      posX = 0;
+      
+    }
+    if (state == '0')
+    {
+      digitalWrite(13, LOW);
+      posX = 110;
+    }
+
+    if (state == '3')
+    {
+      digitalWrite(13, HIGH);
+      posY = 110;
+      
+    }
+    if (state == '2')
+    {
+      digitalWrite(13, LOW);
+      posY = 0;
+    }
+
+    servoX.write(posX);
+    servoY.write(posY);
+    
+
+
+
+  }
+}
